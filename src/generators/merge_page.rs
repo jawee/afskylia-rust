ccd0671177fa12cdd03b01ee2d0ef7462f54299d
Andreas Olsson <olsson.andreas@gmail.com>
2023-02-25 20:49:13 +0100

    replace content in layout works

--- src/generators/merge_page.rs
+++ src/generators/merge_page.rs
@@ -2,12 +2,16 @@
 pub struct MergePage(String);
 
 impl MergePage {
-    pub fn parse(_layout: &str, _content: &str) -> Result<MergePage, String> {
-        if _layout.is_empty() {
+    pub fn parse(layout: &str, content: &str) -> Result<MergePage, String> {
+        if layout.is_empty() {
             return Err(format!("layout is empty"));
         }
 
-        return Ok(Self("".to_string()));
+        let res = layout.replace("{content}", content);
+        return Ok(Self(res.to_string()));
+    }
+    pub fn get_page(&self) -> &str {
+        return &self.0;
     }
 }
 
@@ -16,15 +20,66 @@ mod tests {
     use claim::{assert_ok, assert_err};
     use super::MergePage;
 
+    #[test]
+    fn parse_returns_expected_page()
+    {
+        let layout = r#"
+            <html>
+           <head><title>Hello</title>
+            <body>
+            {content}
+            </body>
+            </html>
+            "#.to_string();
+        let content = "<h1>Hello</h1>".to_string();
+
+        let expected = r#"
+            <html>
+           <head><title>Hello</title>
+            <body>
+            <h1>Hello</h1>
+            </body>
+            </html>
+            "#.to_string();
+
+        let result = MergePage::parse(&layout, &content).unwrap();
+        assert_eq!(result.get_page(), expected);
+    }
 
     #[test]
-    fn valid_layout_is_ok() {
+    fn parse_layout_merged_with_content_ok()
+    {
+        let layout = r#"
+            <html>
+            <head><title>Hello</title>
+            <body>
+            {content}
+            </body>
+            </html>
+            "#.to_string();
+        let content = "<h1>Hello</h1>".to_string();
+
+        let _expected = r#"
+            <html>
+            <head><title>Hello</title>
+            <body>
+            <h1>Hello</h1>
+            </body>
+            </html>
+            "#.to_string();
+
+        assert_ok!(MergePage::parse(&layout, &content));
+    }
+
+    #[test]
+    fn parse_valid_layout_is_ok() {
         let layout = "layout".to_string();
         let content = "".to_string();
         assert_ok!(MergePage::parse(&layout, &content));
     }
+
     #[test]
-    fn empty_layout_is_rejected() {
+    fn parse_empty_layout_is_rejected() {
         let layout = "".to_string();
         let content = "content".to_string();
         assert_err!(MergePage::parse(&layout, &content));
