9a39ff813dbb671ceb4f6ed45f0405651fbba4c7
Andreas Olsson <olsson.andreas@gmail.com>
2023-03-13 20:29:03 +0100

    fragile tests

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -1,3 +1,5 @@
+use std::{path::Path, env::current_dir};
+
 pub fn new(args: &Vec<String>) {
     let command = &args[2];
     // let command = args.iter().nth(2);
@@ -9,21 +11,41 @@ pub fn new(args: &Vec<String>) {
 }
 
 fn new_site() {
-    println!("create new site");
+    let dir = current_dir().unwrap();
+    new_site_internal(dir.as_path());
+}
+
+fn new_site_internal(_dir: &Path) {
+    println!("create new site at {}", _dir.display());
 }
 
 fn new_page(_page_name: &str) {
     println!("create page {}", _page_name);
 }
+
 #[cfg(test)]
 mod tests {
 
     use std::{env::temp_dir, fs::{File, self}, io::Error};
 
+    use super::new_site_internal;
+
+    #[test]
+    fn test_new_site_internal() -> Result<(), Error> {
+        let dir = temp_dir().join("test_tmp_dir2");
+        println!("{}", dir.display());
+        fs::create_dir(dir.as_path())?;
+
+        new_site_internal(dir.as_path());
+        fs::remove_dir_all(dir.as_path())?;
+        return Ok(());
+    }
+
     #[test]
     fn test_tmp_dir() -> Result<(), Error> {
         // let uuid = random
         let dir = temp_dir().join("test_tmp_dir");
+        println!("{}", dir.display());
         fs::create_dir(dir.as_path())?;
         let file_path = dir.as_path().join("tmpfile.txt");
         let _file = File::create(&file_path)?;
