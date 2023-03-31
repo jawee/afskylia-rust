68c16d19ccba34d9e2d68c85a1fb7e02f7ef4b5a
Andreas Olsson <olsson.andreas@gmail.com>
2023-03-31 08:03:57 +0200

    Feature/in memory server (#6)
    * uses content from a HashMap
    
    * initited server module
    
    * mod server
    
    get custom 404 or default if none in content map
    
    * renamed hash_map to content_map
    
    * server tests
    
    * fixed failing tests
    
    * use Vec<u8> instead of String
    
    * removed unnecessary comment
    
    * hadle basic content_type
    
    * handle jpeg
    
    * add to path in a smarter way
    
    * todo
    
    * unit test works
    
    * cleanup test a bit
    
    * refactoring handle_connection test a bit

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,51 +1,63 @@
 use std::{collections::HashMap, net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}};
 
-pub fn start(content_map: HashMap<String, String>) {
+pub fn start(content_map: &HashMap<String, Vec<u8>>) {
     let listener = TcpListener::bind("127.0.0.1:1313").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream, content_map.clone());
+        handle_connection_2(stream, content_map.clone());
     }
 }
 
-fn handle_connection(mut stream: TcpStream, content_map: HashMap<String, String>) {
+fn handle_connection_2(mut stream: TcpStream, content_map: HashMap<String, Vec<u8>>) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    handle_connection(stream, request_line, content_map.clone());
+}
 
-    println!("{}", request_line);
-
-    // GET /asdf HTTP/1.1
+fn handle_connection(mut stream: impl Write, request_line: String, content_map: HashMap<String, Vec<u8>>) {
     let path = get_request_path(&request_line);
 
-    let (status_line, html) = match get_content_for_path(path, content_map.clone()) {
+    let (status_line, content) = match get_content_for_path(path.clone(), content_map.clone()) {
         None => {
-            ("HTTP/1.1 404 NOT FOUND", get_not_found_content(content_map))
+            ("HTTP/1.1 404 NOT FOUND", get_not_found_content(&content_map))
         },
         Some(t) => {
             ("HTTP/1.1 200 OK", t)
         }
     };
 
-    let length = html.len();
+    let content_length = content.len();
+
+    let mut content_type = "text/html";
+    if path.ends_with(".png") {
+        content_type = "image/png";
+    } else if path.ends_with(".css") {
+        content_type = "text/css";
+    } else if path.ends_with(".jpg") || path.ends_with(".jpeg") {
+        content_type = "image/jpeg";
+    }
 
     let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{html}");
+        format!("{status_line}\r\ncontent-length: {content_length}\r\ncontent-type: {content_type}\r\n\r\n");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    let mut respvec = Vec::from(response.as_bytes());
+    respvec.extend_from_slice(&mut Vec::from(content));
+
+    stream.write_all(&respvec).unwrap();
 }
 
-fn get_not_found_content(content_map: HashMap<String, String>) -> String {
+fn get_not_found_content(content_map: &HashMap<String, Vec<u8>>) -> Vec<u8> {
     let content = match content_map.get("404") {
-        Some(t) => t,
-        None => NOT_FOUND 
+        Some(t) => t.to_vec(),
+        None => NOT_FOUND.to_string().as_bytes().to_vec()
     };
     
-    return content.to_string();
+    return content.to_vec();
 }
 
-fn get_content_for_path(path: String, content_map: HashMap<String, String>) -> Option<String> {
+fn get_content_for_path(path: String, content_map: HashMap<String, Vec<u8>>) -> Option<Vec<u8>> {
     let maybe_content = content_map.get(&path).cloned();
     return maybe_content;
 }
@@ -92,47 +104,92 @@ const NOT_FOUND: &str = r#"
 #[cfg(test)]
 mod tests {
 
-    use std::{collections::HashMap, io::Error};
+    use std::{collections::HashMap, io::{Error, Write}};
 
     use crate::server::server::{NOT_FOUND, get_not_found_content};
 
-    use super::{get_request_path, get_request_path_string, get_content_for_path};
+    use super::{get_request_path, get_request_path_string, get_content_for_path, handle_connection};
+
+    struct MockWriter {
+        content: Vec<u8>
+    }
+    
+    impl MockWriter {
+        pub fn get_content(&self) -> Vec<u8> {
+            return self.content.clone();
+        }
+    }
+
+    impl Write for MockWriter {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            self.content.extend_from_slice(buf);
+            return Ok(buf.len());
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            todo!()
+        }
+    }
+    fn get_respvec(status_line: &str, content: &str, content_type: &str) -> Vec<u8> {
+        let content_length = content.len();
+        let response = 
+            format!("{status_line}\r\ncontent-length: {content_length}\r\ncontent-type: {content_type}\r\n\r\n");
+        let mut respvec = Vec::from(response.as_bytes());
+        respvec.extend_from_slice(&mut Vec::from(content));
+        return respvec;
+    }
+
+    #[test]
+    fn test_handle_connection() -> Result<(), Error> {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let content = NOT_FOUND;
+        let content_type = "text/html";
+        let respvec = get_respvec(status_line, content, content_type);
+
+        let mut stream = MockWriter{ content: Vec::new() };
+        let request_line = "".to_string();
+        let content_map: HashMap<String, Vec<u8>> = HashMap::new();
+        handle_connection(&mut stream, request_line, content_map);
+
+        assert_eq!(stream.get_content().len(), respvec.len());
+        return Ok(());
+    }
 
     #[test]
     fn test_get_content_for_path() {
         let path = "/".to_string();
-        let mut content_map: HashMap<String, String> = HashMap::new();
-        content_map.insert("/".to_string(), "content".to_string());
+        let mut content_map: HashMap<String, Vec<u8>> = HashMap::new();
+        content_map.insert("/".to_string(), "content".as_bytes().to_vec());
         let content = get_content_for_path(path, content_map);
 
-        assert_eq!(content, Some("content".to_string()));
+        assert_eq!(content, Some("content".as_bytes().to_vec()));
     }
 
     #[test]
     fn test_get_content_for_path_not_found_none() {
         let path = "/asdf".to_string();
-        let mut content_map: HashMap<String, String> = HashMap::new();
-        content_map.insert("/".to_string(), "content".to_string());
+        let mut content_map: HashMap<String, Vec<u8>> = HashMap::new();
+        content_map.insert("/".to_string(), "content".as_bytes().to_vec());
         let content = get_content_for_path(path, content_map);
 
         assert_eq!(content, None);
     }
-    
+
     #[test]
     fn test_get_not_found_custom_not_found() {
-        let mut content_map: HashMap<String, String> = HashMap::new();
-        content_map.insert("404".to_string(), "content".to_string());
-        let content = get_not_found_content(content_map);
+        let mut content_map: HashMap<String, Vec<u8>> = HashMap::new();
+        content_map.insert("404".to_string(), "content".as_bytes().to_vec());
+        let content = get_not_found_content(&content_map);
 
-        assert_eq!(content, "content".to_string());
+        assert_eq!(content, "content".as_bytes().to_vec());
     }
 
     #[test]
     fn test_get_content_for_path_default_not_found() {
-        let content_map: HashMap<String, String> = HashMap::new();
-        let content = get_not_found_content(content_map);
+        let content_map: HashMap<String, Vec<u8>> = HashMap::new();
+        let content = get_not_found_content(&content_map);
 
-        assert_eq!(content, NOT_FOUND.to_string());
+        assert_eq!(content, NOT_FOUND.as_bytes().to_vec());
     }
 
     #[test]
