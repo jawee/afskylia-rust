89b07d3a08d599de1be6bbd3ce865f17d080570c
Andreas Olsson <olsson.andreas@gmail.com>
2023-04-05 10:18:07 +0200

    read content recursive

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -1,15 +1,51 @@
-use std::{collections::HashMap, env::current_dir, fs::{self, File}, io::{Read, BufReader}, path::{PathBuf, Path}};
+use std::collections::HashMap;
+use std::env::current_dir;
+use std::fs::{self, File};
+use std::io::{Read, BufReader};
+use std::path::PathBuf;
 
 pub fn build(_args: &Vec<String>) {
-    let _curr_dir_path = current_dir().unwrap();
-    todo!("Build is not implemented yet.");
+    let curr_dir_path = current_dir().unwrap();
+    build_internal(curr_dir_path);
 }
 
 fn build_internal(base_dir: PathBuf) {
-    let layouts_map = get_layouts(&base_dir);
+    let _layouts_map = get_layouts(&base_dir);
+    let _content_map = get_content(&base_dir);
+    todo!("Build is not implemented yet.");
 }
 
 static LAYOUT_DIR_PATH: &str = "layouts";
+static CONTENT_DIR_PATH: &str = "content";
+
+fn get_content(base_dir: &PathBuf) -> HashMap<String, String> {
+    let content_dir_path = base_dir.join(CONTENT_DIR_PATH);
+
+    let map = get_content_rec(&content_dir_path, &content_dir_path);
+    return map;
+}
+
+fn get_content_rec(dir: &PathBuf, base_dir: &PathBuf) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    for path in fs::read_dir(&dir).expect("ERROR: Couldn't read content_dir_path") {
+        let path = path.unwrap().path();
+        if path.is_dir(){
+            let new_map = get_content_rec(&path.to_path_buf(), base_dir);
+            map.extend(new_map);
+            continue;
+        }
+
+        let file = File::open(&path).expect("ERROR: Couldn't open file");
+        let mut buf_reader = BufReader::new(file);
+                                                                                                                                                     
+        let mut content = String::new();
+        buf_reader.read_to_string(&mut content).expect("ERROR: Couldn't read file to string");
+
+        let relative_file_path = get_relative_file_path(&path, &base_dir);
+        map.insert(relative_file_path.into_os_string().into_string().expect("ERROR: Couldn't convert path to string"), content);
+    }
+    return map.clone();
+}
 
 fn get_layouts(base_dir: &PathBuf) -> HashMap<String, String> {
     let mut map = HashMap::new();
@@ -27,7 +63,6 @@ fn get_layouts(base_dir: &PathBuf) -> HashMap<String, String> {
 
         let relative_file_path = get_relative_file_path(&path, &layouts_dir_path);
         map.insert(relative_file_path.into_os_string().into_string().expect("ERROR: Couldn't convert path to string"), content);
-        println!("Name: {}", path.display())
     }
 
     return map;
@@ -42,21 +77,34 @@ fn get_relative_file_path(file_path: &PathBuf, base_path: &PathBuf) -> PathBuf {
 
 #[cfg(test)]
 mod tests {
-    use std::{path::{Path, PathBuf}, env::temp_dir, fs::{self, File}, io::{Write, BufWriter}};
+    use std::path::{Path, PathBuf};
+    use std::env::temp_dir;
+    use std::fs::{self, File};
+    use std::io::{Write, BufWriter};
 
     use claim::{assert_ok, assert_some};
     use uuid::Uuid;
 
-    use crate::commands::build::{get_relative_file_path, get_layouts};
+    use crate::commands::build::*;
 
     #[test]
-    fn test_get_layouts_index() {
+    fn test_get_content() {
+        let base_dir = create_test_site();
+        let content_map = get_content(&base_dir);
+
+        assert_ok!(fs::remove_dir_all(base_dir.as_path()));
+        assert_some!(content_map.get("index.md"), "Couldn't get index.md");
+        assert_some!(content_map.get("posts/post-1.md"), "Couldn't get posts/post-1.md");
+        assert_some!(content_map.get("posts/post-2.md"), "Couldn't get posts/post-2.md");
+    }
+    #[test]
+    fn test_get_layouts() {
         let base_dir = create_test_site();
         let layout_map = get_layouts(&base_dir);
 
-        // println!("{:?}", layout_map);
         assert_ok!(fs::remove_dir_all(base_dir.as_path()));
         assert_some!(layout_map.get("index.html"));
+        assert_some!(layout_map.get("posts.html"));
     }
 
     #[test]
@@ -64,7 +112,8 @@ mod tests {
         let base_dir = create_test_site();
         assert_ok!(fs::remove_dir_all(base_dir.as_path()));
     }
-    fn create_test_site() -> PathBuf{
+
+    fn create_test_site() -> PathBuf {
         let uuid = Uuid::new_v4().to_string();
         let base_dir = temp_dir().join(uuid);
         fs::create_dir(base_dir.as_path()).unwrap();
@@ -78,9 +127,23 @@ mod tests {
         let mut buf_writer = BufWriter::new(index_layout_file);
         buf_writer.write(INDEX_LAYOUT.as_ref()).expect("ERROR: couldn't write to layout file");
 
+        let posts_layout_file = File::create(base_dir.as_path().join("layouts/posts.html").as_path()).expect("ERROR: couldn't create posts layout file");
+        buf_writer = BufWriter::new(posts_layout_file);
+        buf_writer.write(INDEX_LAYOUT.as_ref()).expect("ERROR: couldn't write to posts file");
+
         let index_content_file = File::create(base_dir.as_path().join("content").as_path().join("index.md").as_path()).expect("ERROR: couldn't create index content file");
         buf_writer = BufWriter::new(index_content_file);
         buf_writer.write(INDEX_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
+
+        fs::create_dir(base_dir.join("content").join("posts")).expect("ERROR: Couldn't create posts dir in contents");
+
+        let posts_file_1 = File::create(base_dir.join("content").join("posts").join("post-1.md")).expect("Error: couldn't create post-1.md");
+        buf_writer = BufWriter::new(posts_file_1);
+        buf_writer.write(POST_1_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
+
+        let posts_file_2 = File::create(base_dir.join("content").join("posts").join("post-2.md")).expect("Error: couldn't create post-2.md");
+        buf_writer = BufWriter::new(posts_file_2);
+        buf_writer.write(POST_2_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
 
         return base_dir;
     }
@@ -94,8 +157,6 @@ mod tests {
         let res = get_relative_file_path(&file_path, &base_path);
 
         assert_eq!(res.as_path(), Path::new("subdir/index.txt"));
-        // assert_eq!(path.strip_prefix("/test/haha/foo.txt"), Ok(Path::new("")));
-        // assert_eq!(path.strip_prefix("/test/haha/foo.txt/"), Ok(Path::new("")));
     }
     #[test]
     fn test_strip_base_path() {
@@ -105,8 +166,6 @@ mod tests {
         let res = get_relative_file_path(&file_path, &base_path);
 
         assert_eq!(res.as_path(), Path::new("index.txt"));
-        // assert_eq!(path.strip_prefix("/test/haha/foo.txt"), Ok(Path::new("")));
-        // assert_eq!(path.strip_prefix("/test/haha/foo.txt/"), Ok(Path::new("")));
     }
 
 
@@ -115,6 +174,16 @@ mod tests {
         # Index
 
         Some content
+        "#;
+    static POST_1_CONTENT: &str = r#"
+        # Post 1
+
+        Post content
+        "#;
+    static POST_2_CONTENT: &str = r#"
+        # Post 2
+
+        Post content
         "#;
     static INDEX_LAYOUT: &str = r#"
         <!DOCTYPE html>
