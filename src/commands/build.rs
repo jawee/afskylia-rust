58e2780a4fc1ffe5917e0ad4ef252719a86ba394
Andreas Olsson <olsson.andreas@gmail.com>
2023-04-07 13:27:43 +0200

    Feature/handle resources (#22)
    * copy resources to public
    
    * extracted create_test_site from tests module
    
    * using SiteBuilder for building test sites
    
    * SiteBuilder creates temp directory
    
    * refactor unit tests
    
    * removed unused method

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::env::current_dir;
 use std::fs::{self, File};
 use std::io::{Read, BufReader, BufWriter, Write};
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use crate::generators::{MergePage, HtmlGenerator};
 use crate::parsers::markdown::Lexer;
@@ -21,6 +21,8 @@ fn build_internal(base_dir: &PathBuf) -> HashMap<PathBuf, String> {
     if !public_dir_path.is_dir() {
         fs::create_dir(&public_dir_path).expect("ERROR: Couldn't create public dir");
     }
+
+    copy_resources_to_public(&base_dir.join("resources"), &public_dir_path);
 
     let maybe_base_template = layouts_map.remove("_base.html");
     let keys = layouts_map.clone().into_keys().collect::<Vec<String>>();
@@ -64,6 +66,24 @@ fn build_internal(base_dir: &PathBuf) -> HashMap<PathBuf, String> {
     return HashMap::new();
 }
 
+fn copy_resources_to_public(resources_dir: &PathBuf, public_dir: &PathBuf) {
+    if !Path::new(public_dir).exists() {
+        fs::create_dir_all(public_dir).expect("ERROR: Couldn't create public dir");
+    }
+    for entry in fs::read_dir(resources_dir).expect("ERROR: Couldn't read resources dir") {
+        match entry {
+            Ok(entry) => {
+                let orig_path = entry.path();
+                let new_path = public_dir.join(&orig_path.file_name().expect("ERROR: Couldn't get file_name"));
+                fs::copy(orig_path, new_path).expect("ERROR: couldn't copy file");
+            },
+            Err(e) => {
+                eprintln!("ERROR: Couldn't read resource: {e}")
+            }
+        }
+    }
+}
+
 fn merge_base_with_layout(base_tpl: &Option<String>, content_layout: &String) -> String {
     let layout = match base_tpl {
         Some(base) => {
@@ -104,7 +124,7 @@ fn build_menu_html(pages: Vec<String>) -> String {
 fn get_content_rec(dir: &PathBuf, base_dir: &PathBuf) -> HashMap<String, String> {
     let mut map = HashMap::new();
     for path in fs::read_dir(&dir).expect("ERROR: Couldn't read content_dir_path") {
-        let path = path.unwrap().path();
+        let path = path.expect("ERROR: couldn't get path").path();
         if path.is_dir(){
             let new_map = get_content_rec(&path.to_path_buf(), base_dir);
             map.extend(new_map);
@@ -152,14 +172,29 @@ fn get_relative_file_path(file_path: &PathBuf, base_path: &PathBuf) -> PathBuf {
 #[cfg(test)]
 mod tests {
     use std::path::{Path, PathBuf};
-    use std::env::temp_dir;
-    use std::fs::{self, File};
-    use std::io::{Write, BufWriter};
 
     use claim::{assert_ok, assert_some};
-    use uuid::Uuid;
 
+    use crate::commands::test_utils::*;
     use crate::commands::build::*;
+
+    #[test]
+    fn test_copy_resources() {
+        let site_builder = SiteBuilder::new()
+            .with_resource("style", "css", "")
+            .with_resource("script", "js", "");
+
+        let base_dir_path = site_builder.get_path();
+
+        let public_dir_path = base_dir_path.join("public");
+
+        copy_resources_to_public(&base_dir_path.join("resources"), &public_dir_path);
+
+        assert_eq!(Path::new(&public_dir_path.join("style.css")).exists(), true, "style.css doesn't exist");
+        assert_eq!(Path::new(&public_dir_path.join("script.js")).exists(), true, "script.js doesn't exist");
+
+        assert_ok!(fs::remove_dir_all(base_dir_path.as_path()));
+    }
 
     #[test]
     fn test_build_menu() {
@@ -169,9 +204,14 @@ mod tests {
         let result = build_menu_html(menu_items);
         assert_eq!(result, expected);
     }
+
     #[test]
     fn test_build_internal() {
-        let base_dir_path = create_test_site();
+        let site_builder = SiteBuilder::new()
+            .with_base_layout("base", BASE)
+            .with_page_with_content("index", INDEX_LAYOUT, INDEX_CONTENT);
+        let base_dir_path = site_builder.get_path();
+
         build_internal(&base_dir_path);
 
         let public_dir_path = base_dir_path.join(PUBLIC_DIR_PATH);
@@ -185,63 +225,48 @@ mod tests {
     }
 
     #[test]
-    fn test_get_content() {
-        let base_dir = create_test_site();
-        let content_map = get_content(&base_dir);
+    fn test_get_content_nested() {
+        let site_builder = SiteBuilder::new()
+            .with_base_layout("base", BASE)
+            .with_page_with_content("index", INDEX_LAYOUT, INDEX_CONTENT)
+            .with_page_with_nested_content("posts", INDEX_LAYOUT, PathBuf::from("posts"), "post-1", POST_1_CONTENT)
+            .with_nested_content(PathBuf::from("posts"), "post-2", POST_2_CONTENT);
+        let base_dir_path = site_builder.get_path();
 
-        assert_ok!(fs::remove_dir_all(base_dir.as_path()));
+        let content_map = get_content(&base_dir_path);
+
+        assert_ok!(fs::remove_dir_all(base_dir_path.as_path()));
         assert_some!(content_map.get("index.md"), "Couldn't get index.md");
         assert_some!(content_map.get("posts/post-1.md"), "Couldn't get posts/post-1.md");
         assert_some!(content_map.get("posts/post-2.md"), "Couldn't get posts/post-2.md");
     }
-    #[test]
-    fn test_get_layouts() {
-        let base_dir = create_test_site();
-        let layout_map = get_layouts(&base_dir);
 
-        assert_ok!(fs::remove_dir_all(base_dir.as_path()));
-        assert_some!(layout_map.get("index.html"));
-        assert_some!(layout_map.get("posts.html"));
+    #[test]
+    fn test_get_content() {
+        let site_builder = SiteBuilder::new()
+            .with_base_layout("base", BASE)
+            .with_page_with_content("index", INDEX_LAYOUT, INDEX_CONTENT);
+        let base_dir_path = site_builder.get_path();
+
+        let content_map = get_content(&base_dir_path);
+
+        assert_ok!(fs::remove_dir_all(base_dir_path.as_path()));
+        assert_some!(content_map.get("index.md"), "Couldn't get index.md");
     }
 
-    fn create_test_site() -> PathBuf {
-        let uuid = Uuid::new_v4().to_string();
-        let base_dir = temp_dir().join("rust").join(uuid);
-        fs::create_dir_all(base_dir.as_path()).expect("ERROR: couldn't create base_dir");
+    #[test]
+    fn test_get_layouts() {
+        let site_builder = SiteBuilder::new()
+            .with_base_layout("base", BASE)
+            .with_page_with_content("index", INDEX_LAYOUT, INDEX_CONTENT)
+            .with_page_with_nested_content("posts", INDEX_LAYOUT, PathBuf::from("posts"), "post-1", POST_1_CONTENT);
 
-        let dirs = vec!["content", "layouts", "resources"];
-        for dir in dirs {
-            fs::create_dir(base_dir.join(dir).as_path()).unwrap();
-        }
+        let base_dir_path = site_builder.get_path();
+        let layout_map = get_layouts(&base_dir_path);
 
-        let index_layout_file = File::create(base_dir.join("layouts").join("index.html")).expect("ERROR: couldn't create index layout file");
-        let mut buf_writer = BufWriter::new(index_layout_file);
-        buf_writer.write(INDEX_LAYOUT.as_ref()).expect("ERROR: couldn't write to layout file");
-
-        let base_layout_file = File::create(base_dir.join("layouts").join("_base.html")).expect("ERROR: couldn't create base layout file");
-        buf_writer = BufWriter::new(base_layout_file);
-        buf_writer.write(BASE.as_ref()).expect("ERROR: couldn't write to content file");
-
-
-        let index_content_file = File::create(base_dir.join("content").join("index.md")).expect("ERROR: couldn't create index content file");
-        buf_writer = BufWriter::new(index_content_file);
-        buf_writer.write(INDEX_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
-
-        let posts_layout_file = File::create(base_dir.as_path().join("layouts/posts.html").as_path()).expect("ERROR: couldn't create posts layout file");
-        buf_writer = BufWriter::new(posts_layout_file);
-        buf_writer.write(INDEX_LAYOUT.as_ref()).expect("ERROR: couldn't write to posts file");
-
-        fs::create_dir(base_dir.join("content").join("posts")).expect("ERROR: Couldn't create posts dir in contents");
-
-        let posts_file_1 = File::create(base_dir.join("content").join("posts").join("post-1.md")).expect("Error: couldn't create post-1.md");
-        buf_writer = BufWriter::new(posts_file_1);
-        buf_writer.write(POST_1_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
-
-        let posts_file_2 = File::create(base_dir.join("content").join("posts").join("post-2.md")).expect("Error: couldn't create post-2.md");
-        buf_writer = BufWriter::new(posts_file_2);
-        buf_writer.write(POST_2_CONTENT.as_ref()).expect("ERROR: couldn't write to content file");
-
-        return base_dir;
+        assert_ok!(fs::remove_dir_all(base_dir_path.as_path()));
+        assert_some!(layout_map.get("index.html"));
+        assert_some!(layout_map.get("posts.html"));
     }
 
 
@@ -254,6 +279,7 @@ mod tests {
 
         assert_eq!(res.as_path(), Path::new("subdir/index.txt"));
     }
+
     #[test]
     fn test_strip_base_path() {
         let base_path = PathBuf::from("/home/user/website/");
@@ -263,25 +289,110 @@ mod tests {
 
         assert_eq!(res.as_path(), Path::new("index.txt"));
     }
+}
 
+#[cfg(test)]
+pub mod test_utils {
+    use std::io::{BufWriter, Write};
+    use std::fs::{self, File};
+    use std::env::temp_dir;
+    use std::path::PathBuf;
 
+    use uuid::Uuid;
+    pub struct SiteBuilder {
+        base_dir_path: PathBuf
+    }
 
-    static INDEX_CONTENT: &str = "\
+    impl SiteBuilder {
+        pub fn new() -> Self {
+            let uuid = Uuid::new_v4().to_string();
+            let base_dir_path = temp_dir().join("rust").join(uuid);
+            fs::create_dir_all(base_dir_path.as_path()).expect("ERROR: couldn't create base_dir");
+            let dirs = vec!["content", "layouts", "resources"];
+            for dir in dirs {
+                fs::create_dir_all(base_dir_path.join(dir)).expect("ERROR: Couldn't create dir");
+            }
+            return SiteBuilder {base_dir_path};
+        }
+
+        pub fn get_path(self) -> PathBuf {
+            return self.base_dir_path.clone();
+        }
+
+        pub fn with_base_layout(self, layout_name: &str, layout_content: &str) -> Self {
+            let layout_file_name = format!("_{layout_name}.html");
+            let base_layout_file = File::create(self.base_dir_path.join("layouts").join(layout_file_name)).expect("ERROR: couldn't create base layout file");
+            let mut buf_writer = BufWriter::new(base_layout_file);
+            buf_writer.write(layout_content.as_ref()).expect("ERROR: couldn't write to content file");
+
+            return self;
+        }
+
+        pub fn with_resource(self, resource_name: &str, resource_file_extension: &str,  resource_content: &str) -> Self {
+            let resource_file_name = format!("{}.{}", resource_name, resource_file_extension);
+            let resource_file = File::create(self.base_dir_path.join("resources").join(resource_file_name)).expect("ERROR: couldn't create resources file");
+            let mut buf_writer = BufWriter::new(resource_file);
+            buf_writer.write(resource_content.as_ref()).expect("ERROR: couldn't write to resource file");
+            return self;
+        }
+
+        pub fn with_page_with_content(self, page_name: &str, page_layout: &str, page_content: &str) -> Self {
+            //layout
+            let layout_file_name = format!("{}.html", page_name);
+            let layout_file = File::create(self.base_dir_path.join("layouts").join(layout_file_name)).expect("ERROR: couldn't create layout file");
+            let mut buf_writer = BufWriter::new(layout_file);
+            buf_writer.write(page_layout.as_ref()).expect("ERROR: couldn't write to layout file");
+
+            //content
+            let content_file_name = format!("{}.md", page_name);
+            let content_file = File::create(self.base_dir_path.join("content").join(content_file_name)).expect("Error: couldn't content file");
+            buf_writer = BufWriter::new(content_file);
+            buf_writer.write(page_content.as_ref()).expect("ERROR: couldn't write to content file");
+            return self;
+        }
+
+        pub fn with_page_with_nested_content(self, page_name: &str, page_layout: &str, page_content_path: PathBuf, page_content_name: &str, page_content: &str) -> Self {
+            fs::create_dir_all(self.base_dir_path.join("content").join(&page_content_path)).expect("ERROR: Couldn't create page_content_path");
+
+            //layout
+            let layout_file_name = format!("{}.html", page_name);
+            let layout_file = File::create(self.base_dir_path.join("layouts").join(layout_file_name)).expect("ERROR: couldn't create layout file");
+            let mut buf_writer = BufWriter::new(layout_file);
+            buf_writer.write(page_layout.as_ref()).expect("ERROR: couldn't write to layout file");
+
+            //content
+            let content_file_name = format!("{}.md", page_content_name);
+            let content_file = File::create(self.base_dir_path.join("content").join(page_content_path).join(content_file_name)).expect("Error: couldn't create content file");
+            buf_writer = BufWriter::new(content_file);
+            buf_writer.write(page_content.as_ref()).expect("ERROR: couldn't write to content file");
+            return self;
+        }
+
+        pub fn with_nested_content(self, content_path: PathBuf, content_name: &str, content: &str) -> Self {
+            let content_file_name = format!("{}.md", content_name);
+            let content_file = File::create(self.base_dir_path.join("content").join(&content_path).join(content_file_name)).expect("Error: couldn't create content file");
+            let mut buf_writer = BufWriter::new(content_file);
+            buf_writer.write(content.as_ref()).expect("ERROR: couldn't write to content file");
+            return self;
+        }
+    }
+
+    pub static INDEX_CONTENT: &str = "\
         # Index\n\
         \n\
         Some content\n\
         ";
-    static POST_1_CONTENT: &str = "\
+    pub static POST_1_CONTENT: &str = "\
         # Post 1\n\
         \n\
         Post content\n\
         ";
-    static POST_2_CONTENT: &str = "\
+    pub static POST_2_CONTENT: &str = "\
         # Post 2\n\
         \n\
         Post content\n\
         ";
-    static BASE: &str = r#"
+    pub static BASE: &str = r#"
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -294,8 +405,7 @@ mod tests {
         </body>
         </html>
         "#;
-    static INDEX_LAYOUT: &str = r#"
+    pub static INDEX_LAYOUT: &str = r#"
         {content}
         "#;
-
 }
