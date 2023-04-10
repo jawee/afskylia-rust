b04f1acd87ed9efac7f27784be9c49ac73c58490
Andreas Olsson <olsson.andreas@gmail.com>
2023-04-10 14:44:26 +0200

    timestamp works

--- src/parsers/metadata/mod.rs
+++ src/parsers/metadata/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, collections::HashMap};
 
 use chrono::prelude::*;
 
@@ -8,6 +8,147 @@ pub struct Metadata {
     published: bool,
 }
 
+pub struct MyDateTime {
+    timestamp: usize,
+    day: usize,
+    month: usize,
+    year: usize,
+    hour: usize,
+    minute: usize,
+    second: usize
+}
+
+impl MyDateTime {
+
+    pub fn new(timestamp: usize) -> Self {
+        let map = Self::parse_timestamp(timestamp);
+
+        let second = *map.get("second").expect("ERROR: Couldn't get second");
+        let minute = *map.get("minute").expect("ERROR: Couldn't get minute");
+        let hour = *map.get("hour").expect("ERROR: Couldn't get hour");
+        let day = *map.get("day").expect("ERROR: Couldn't get day");
+        let month = *map.get("month").expect("ERROR: Couldn't get month");
+        let year = *map.get("year").expect("ERROR: Couldn't get year");
+
+        return MyDateTime { timestamp, second, minute, hour, day, month, year };
+    }
+
+    fn parse_timestamp_2(timestamp: usize) -> HashMap<String, usize> {
+        let second = timestamp % 60;
+        let minute = (timestamp / 60) % 60;
+        let hour = (timestamp / 60 / 60) % 24;
+        let day = timestamp / 60 / 60 / 24;
+        let years = (day as f64 / 365.25) as usize;
+        let year = 1970 + years;
+
+
+        let mut days = day - years * 365;// - leapdays
+
+        let mut ly = 0;
+        if year % 4 == 0 {
+            ly = 1;
+        } else {
+            ly = 0;
+        }
+
+        let mut month = 0;
+        while month <= 12 {
+            month += 1;
+            let mut days_in_month = 0;
+            if month == 2 {
+                let leap_year = if !(year % 4 == 0) { 1 } else { 0 };
+                let leap_year_2 = if !(year % 100 == 0) { 1 } else { 0 };
+                let leap_year_3= if !(year % 400 == 0) { 1 } else { 0 };
+                days_in_month = 28 + leap_year + leap_year_2 + leap_year_3;
+            } else {
+                let extra_day = if month < 7 { 1 } else { 0 };
+                days_in_month = 30 + ((month + extra_day) % 2);
+            }
+
+            if days > days_in_month {
+                days = days - days_in_month;
+            }
+        }
+
+        let mut map = HashMap::default();
+        map.insert(String::from("hour"), hour);
+        map.insert(String::from("minute"), minute);
+        map.insert(String::from("second"), second);
+        map.insert(String::from("year"), year);
+        map.insert(String::from("month"), month);
+        map.insert(String::from("day"), day);
+        return map;
+    }
+    fn parse_timestamp(timestamp: usize) -> HashMap<String, usize> {
+        let mut map = HashMap::default();
+        let days_since_1970 = timestamp/ 86400;
+        let hour = ((timestamp - days_since_1970 * 86400) / 3600) % 24;
+        let minute = (((timestamp - days_since_1970 * 86400) - hour*3600) / 60) % 60; 
+        let second = ((((timestamp - days_since_1970 * 86400) - hour * 3600) - minute * 60)) % 60;
+
+        let q_years = days_since_1970 / 1461;
+        let year = q_years * 4 + (days_since_1970 - (q_years * 1461)) / 365;
+
+        let mut days_this_year = (days_since_1970 - (q_years * 1461)) % 365;
+
+        let mut leap_year = false;
+        if year % 4 == 2 {
+            leap_year = true;
+            days_this_year += 1;
+        }
+
+        let days_in_month: [usize; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let mut month = 0;
+        while days_this_year > days_in_month[month] {
+            let mut days_this_month = days_in_month[month];
+            if month == 1 && leap_year {
+                days_this_month += 1;
+            }
+            days_this_year -= days_this_month;
+            month += 1;
+        }
+
+        let day = days_this_year+1;
+        map.insert(String::from("hour"), hour);
+        map.insert(String::from("minute"), minute);
+        map.insert(String::from("second"), second);
+        map.insert(String::from("year"), 1970+year);
+        map.insert(String::from("month"), 1+month);
+        map.insert(String::from("day"), day);
+        return map;
+    }
+}
+
+impl Display for MyDateTime {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:04}-{:02}-{:02}T{:02}:{:02}:{:02}", self.year, self.month, self.day, self.hour, self.minute, self.second)
+    }
+}
+
+#[cfg(test)]
+mod mydatetimetests {
+    use super::MyDateTime;
+
+    #[test]
+    fn convert_to_string() {
+        let input: usize = 1681105217;
+        let expected = "2023-04-10T05:40:17";
+
+        let date_time = MyDateTime::new(input);
+        let result = date_time.to_string();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn convert_to_string_2() {
+        let input: usize = 1313905026;
+        let expected = "2011-08-21T05:37:06";
+
+        let date_time = MyDateTime::new(input);
+        let result = date_time.to_string();
+        assert_eq!(result, expected);
+    }
+}
 impl Default for Metadata {
     fn default() -> Self {
         return Metadata {
